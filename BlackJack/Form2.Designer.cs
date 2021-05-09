74074d9ad50f223d72ab1acd5d8ce52e4b34eaf3
Weslley Santos <weslleygoncalves1901@hotmail.com>
2021-05-08 23:51:53 -0300

    revisao, atv.4_Semestre_TEC_PRG

---
+++ BlackJack/Form2.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace BlackJack
+{
+    partial class form_apresentacao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_apresentacao));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_Iniciar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.MintCream;
+            this.pictureBox1.Image = global::BlackJack.Properties.Resources.blackonline;
+            this.pictureBox1.Location = new System.Drawing.Point(-9, -23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(361, 483);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btn_Iniciar
+            // 
+            this.btn_Iniciar.BackColor = System.Drawing.Color.Azure;
+            this.btn_Iniciar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_Iniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Iniciar.FlatAppearance.BorderColor = System.Drawing.Color.Azure;
+            this.btn_Iniciar.FlatAppearance.BorderSize = 5;
+            this.btn_Iniciar.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_Iniciar.ForeColor = System.Drawing.Color.Black;
+            this.btn_Iniciar.Location = new System.Drawing.Point(96, 414);
+            this.btn_Iniciar.Name = "btn_Iniciar";
+            this.btn_Iniciar.Size = new System.Drawing.Size(153, 29);
+            this.btn_Iniciar.TabIndex = 2;
+            this.btn_Iniciar.Text = "INICIAR";
+            this.btn_Iniciar.UseVisualStyleBackColor = false;
+            this.btn_Iniciar.Click += new System.EventHandler(this.btn_Iniciar_Click);
+            // 
+            // form_apresentacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_Iniciar);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "form_apresentacao";
+            this.Text = "BlackJack (Duplas)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_Iniciar;
+    }
+}
